29075b915bce292251fbd84b11f14b456f3bf8f1
Haoran Zhang <haoranz5@andrew.cmu.edu>
2024-12-05 11:25:16 -0500

    fix a small problem

--- train_all.sh
+++ train_all.sh
@@ -4,10 +4,10 @@
 iidalpha=0.1 # lower value means more non-iid (more challenging)
 NumClients=50
 
-python plots.py --samplingAlgo bandit_sampling --num-clients $NumClients --iid_alpha $iidalpha
-python plots.py --samplingAlgo full_participation_train --num-clients $NumClients --iid_alpha $iidalpha
-python plots.py --samplingAlgo random_sampling_train --num-clients $NumClients --iid_alpha $iidalpha # uniform distribution
-python plots.py --samplingAlgo dataset_size_sampling_train --num-clients $NumClients --iid_alpha $iidalpha
-python plots.py --samplingAlgo variance_reduced_sampling_train --num-clients $NumClients --iid_alpha $iidalpha
-python plots.py --samplingAlgo poc_sampling --num-clients $NumClients --iid_alpha $iidalpha
-python plots.py --samplingAlgo random_sampling --num-clients $NumClients --iid_alpha $iidalpha # random, fixed active client number
+python plots.py --samplingAlgo bandit_sampling --num-clients $NumClients --iid-alpha $iidalpha
+python plots.py --samplingAlgo full_participation_train --num-clients $NumClients --iid-alpha $iidalpha
+python plots.py --samplingAlgo random_sampling_train --num-clients $NumClients --iid-alpha $iidalpha # uniform distribution
+python plots.py --samplingAlgo dataset_size_sampling_train --num-clients $NumClients --iid-alpha $iidalpha
+python plots.py --samplingAlgo variance_reduced_sampling_train --num-clients $NumClients --iid-alpha $iidalpha
+python plots.py --samplingAlgo poc_sampling --num-clients $NumClients --iid-alpha $iidalpha
+python plots.py --samplingAlgo random_sampling --num-clients $NumClients --iid-alpha $iidalpha # random, fixed active client number
